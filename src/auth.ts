719daa2c85984f268f4fccf36a17a7c8c4fe0b90
sanskar001 <sanskarm001@gmail.com>
2025-05-21 23:42:47 +0530

    chore: install prisma and setup with auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,20 @@
 import NextAuth from "next-auth";
+import { prisma } from "./prisma";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import Credentials from "next-auth/providers/credentials";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google, GitHub],
+  providers: [
+    Google,
+    GitHub,
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
 });
